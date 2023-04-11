9e1e1f0d7e87cbc284799718e612f9c7776cb6d3
Mohit Bhatt <mohit.peak@gmail.com>
2023-04-11 13:35:07 +0530

    body-parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const { PORT } = require ('./config/serverConfig'); 
 
 const setupAndStartServer = async () => {
@@ -6,6 +8,12 @@ const setupAndStartServer = async () => {
   // create the express object
 
   const app = express();
+
+    // add middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
   app.listen(PORT, () => {
     console.log(`Server started on port`, PORT);
   });
