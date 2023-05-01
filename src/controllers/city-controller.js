0e266653d6e3cf5cd2333c65010317cd130b6895
Mohit Bhatt <mohit.peak@gmail.com>
2023-05-01 20:54:55 +0530

    Added filters to getAllCities

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -2,145 +2,135 @@ const { CityService } = require('../services/index');
 
 const cityService = new CityService();
 
-
 /**
- * 
+ *
  * POST request
  * data -> req.body
  */
 
 const create = async (req, res) => {
-    try {
-        const city = await cityService.createCity(req.body);
-        return res.status(201).json({
-            data : city,
-            success: true,
-            message: "City created successfully",
-            err : {}
-        });
-
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data : {},
-            success: false,
-            message: "Some error occured while creating city",
-            err : error
-        });
-    }
-}
+  try {
+    const city = await cityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: 'City created successfully',
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Some error occured while creating city',
+      err: error,
+    });
+  }
+};
 
 /*
-    * PUT request
-    * data -> req.body
-    * id -> req.params.id
-*/
+ * PUT request
+ * data -> req.body
+ * id -> req.params.id
+ */
 
 const update = async (req, res) => {
-    try {
-        const cityId = req.params.id;
-        const city = await cityService.updateCity(cityId, req.body);
-        return res.status(200).json({
-            data : city,
-            success: true,
-            message: "City updated successfully",
-            err : {}
-        });
-
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data : {},
-            success: false,
-            message: "Some error occured while updating city",
-            err : error
-        });
-    }
-}
+  try {
+    const cityId = req.params.id;
+    const city = await cityService.updateCity(cityId, req.body);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: 'City updated successfully',
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Some error occured while updating city',
+      err: error,
+    });
+  }
+};
 
 /*
-    * DELETE request
-    * id -> req.params.id
-*/
+ * DELETE request
+ * id -> req.params.id
+ */
 
 const destroy = async (req, res) => {
-    try {
-        const cityId = req.params.id;
-        const response = await cityService.deleteCity(cityId);
-        return res.status(200).json({
-            data : response,
-            success: true,
-            message: "City deleted successfully",
-            err : {}
-        });
-
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data : {},
-            success: false,
-            message: "Some error occured while deleting city",
-            err : error
-        });
-    }
-}
+  try {
+    const cityId = req.params.id;
+    const response = await cityService.deleteCity(cityId);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: 'City deleted successfully',
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Some error occured while deleting city',
+      err: error,
+    });
+  }
+};
 
 /*
-    * GET request
-    * id -> req.params.id
-*/ 
+ * GET request
+ * id -> req.params.id
+ */
 
 const get = async (req, res) => {
-    try {
-        const cityId = req.params.id;
-        const response = await cityService.getCity(cityId);
-        return res.status(200).json({
-            data : response,
-            success: true,
-            message: "City fetched successfully",
-            err : {}
-        });
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data : {},
-            success: false,
-            message: "Some error occured while fetching city",
-            err : error
-        });
-    }
-}
+  try {
+    const cityId = req.params.id;
+    const response = await cityService.getCity(cityId);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: 'City fetched successfully',
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Some error occured while fetching city',
+      err: error,
+    });
+  }
+};
 
 const getAll = async (req, res) => {
-    try {
-        const response = await cityService.getAllCities();
-        return res.status(200).json({
-            data : response,
-            success: true,
-            message: "Cities fetched successfully",
-            err : {}
-        });
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data : {},
-            success: false,
-            message: "Some error occured while fetching cities",
-            err : error
-        });
-    }
-}
-
+  try {
+    const response = await cityService.getAllCities(req.query);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: 'Cities fetched successfully',
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Some error occured while fetching cities',
+      err: error,
+    });
+  }
+};
 
 module.exports = {
-    create,
-    update,
-    destroy,
-    get,
-    getAll
-}
+  create,
+  update,
+  destroy,
+  get,
+  getAll,
+};
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -1,59 +1,61 @@
-const {CityRepository} = require ('../repository/index');
+const { CityRepository } = require('../repository/index');
 
 class CityService {
-    constructor() {
-        this.cityRepository = new CityRepository();
-    }
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
 
-    async createCity (data) {
-        try {
-            const city = await this.cityRepository.createCity({name: data.name});
-            return city;
-        } catch (error) {
-            console.log("Some error occured in service while creating city");
-            throw {error};
-        }
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity({ name: data.name });
+      return city;
+    } catch (error) {
+      console.log('Some error occured in service while creating city');
+      throw { error };
     }
+  }
 
-    async deleteCity (cityId) {
-        try {
-            const response = await this.cityRepository.deleteCity(cityId);
-            return response;
-        } catch (error) {
-            console.log("Some error occured in service while deleting city");
-            throw {error};
-        }
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId);
+      return response;
+    } catch (error) {
+      console.log('Some error occured in service while deleting city');
+      throw { error };
     }
+  }
 
-    async updateCity (cityId, data) {
-        try {
-            const city = await this.cityRepository.updateCity(cityId, data);
-            return city;
-        } catch (error) {
-            console.log("Some error occured in service while updating city");
-            throw {error};
-        }
+  async updateCity(cityId, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } catch (error) {
+      console.log('Some error occured in service while updating city');
+      throw { error };
     }
+  }
 
-    async getCity (cityId) {
-        try {
-            const city = await this.cityRepository.getCity(cityId);
-            return city;
-        } catch (error) {
-            console.log("Some error occured in service while getting city");
-            throw {error};
-        }
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId);
+      return city;
+    } catch (error) {
+      console.log('Some error occured in service while getting city');
+      throw { error };
     }
+  }
 
-    async getAllCities () {
-        try {
-            const cities = await this.cityRepository.getAllCities();
-            return cities;
-        } catch (error) {
-            console.log("Some error occured in service while getting all cities");
-            throw {error};
-        }
+  async getAllCities(filter) {
+    try {
+      const cities = await this.cityRepository.getAllCities({
+        name: filter.name,
+      });
+      return cities;
+    } catch (error) {
+      console.log('Some error occured in service while getting all cities');
+      throw { error };
     }
+  }
 }
 
 module.exports = CityService;
